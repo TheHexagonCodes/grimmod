9fd82c9eb56b2a5a9a26b3cc7fd0e7d23fb4a01a
Hexagon Codes <the@hexagon.codes>
2023-04-28 18:35:46 +0100

    build: output the dll as "DebugLog.dll"
    GrimFandango loads this dll and handily does nothing if
    "_ShowDebugLog" and "_logPrintf" are not found within it.

---
+++ build.rs
@@ -0,0 +1,10 @@
+use std::env;
+
+fn main() {
+    let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let profile = env::var("PROFILE").unwrap();
+    println!(
+        "cargo:rustc-cdylib-link-arg=/OUT:{}\\target\\{}\\DebugLog.dll",
+        project_dir, profile
+    );
+}
