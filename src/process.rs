f4a67ae6b3dc9b92047d69ad137f180bb6668a54
Hexagon Codes <the@hexagon.codes>
2023-09-21 13:23:24 +0100

    feat: add functions to read & write memory

--- src/process.rs
+++ src/process.rs
@@ -1,5 +1,6 @@
 use windows::core::PCWSTR;
 use windows::Win32::Foundation::BOOL;
+use windows::Win32::System::Diagnostics::Debug::{ReadProcessMemory, WriteProcessMemory};
 use windows::Win32::System::LibraryLoader::GetModuleHandleW;
 use windows::Win32::System::ProcessStatus::{GetModuleInformation, MODULEINFO};
 use windows::Win32::System::Threading::GetCurrentProcess;
@@ -29,6 +30,41 @@ impl Process {
                 std::mem::size_of::<MODULEINFO>() as u32,
             );
             (result == BOOL(1)).then_some(module_info.lpBaseOfDll as usize)
+        }
+    }
+
+    pub fn read<T: Sized>(&self, offset: usize) -> T {
+        let address = self.base_address + offset;
+
+        unsafe {
+            let process_handle = GetCurrentProcess();
+            let mut value: T = std::mem::zeroed();
+
+            ReadProcessMemory(
+                process_handle,
+                address as *mut _,
+                &mut value as *mut T as *mut _,
+                std::mem::size_of::<T>(),
+                None,
+            );
+
+            value
+        }
+    }
+
+    pub fn write<T: Sized>(&self, offset: usize, value: T) {
+        let address = self.base_address + offset;
+        let size = std::mem::size_of::<T>();
+
+        unsafe {
+            let process_handle = GetCurrentProcess();
+            let _result = WriteProcessMemory(
+                process_handle,
+                address as *mut _,
+                &value as *const T as *const _,
+                size,
+                None,
+            );
         }
     }
 }
