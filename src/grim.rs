1402be739f84b531a34d015ab1b300c62328ccf6
Hexagon Codes <the@hexagon.codes>
2023-10-07 00:21:36 +0100

    feat: add struct and mutator for runtime configed functions

--- src/grim.rs
+++ src/grim.rs
@@ -1,3 +1,5 @@
+use crate::process;
+
 use std::ffi::{c_char, c_int, c_void};
 use std::mem;
 
@@ -6,6 +8,9 @@ pub mod addresses {
     pub const OPEN_FILE: usize = 0x0034EF80;
     pub const CLOSE_FILE: usize = 0x0034C870;
     pub const READ_FILE: usize = 0x0034E050;
+
+    // contains the address for the RuntimeContext in use by the game
+    pub const RUNTIME_CONTEXT_PTR: usize = 0x034E2CD8;
 }
 
 type FileOpener = extern "C" fn(*mut c_char, *mut c_char) -> *mut c_void;
@@ -33,5 +38,26 @@ pub fn read_file(file: *mut c_void, dst: *mut c_void, size: usize) -> usize {
     unsafe {
         let f: FileReader = mem::transmute(addresses::READ_FILE);
         f(file, dst, size)
+    }
+}
+
+/// Stores a set of IO functions the game uses at runtime
+///
+/// This is used to make what functions are used at runtime configurable.
+/// e.g. read files from a simple directory in debug but from LAB archives in release.
+#[repr(C)]
+pub struct RuntimeContext {
+    _ignore_pre: [u32; 12],
+    pub open_file: *const FileOpener,
+    pub close_file: *const FileCloser,
+    pub read_file: *const FileReader,
+    _ignore_post: [u32; 17],
+}
+
+#[inline(always)]
+pub fn with_runtime_context<F: FnOnce(&mut RuntimeContext)>(f: F) {
+    unsafe {
+        let runtime_context = process::read::<usize>(addresses::RUNTIME_CONTEXT_PTR);
+        process::with_mut_ref(runtime_context, f)
     }
 }
