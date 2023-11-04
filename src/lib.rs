03380e4c9d390b8cf738bf1e6a162d449d94a0b0
Hexagon Codes <the@hexagon.codes>
2023-11-04 15:40:44 +0000

    feat: init process base address with global mutex

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 mod debug;
 mod process;
 
-use process::Process;
-
 use std::ffi::c_void;
 use windows::Win32::Foundation::BOOL;
 use windows::Win32::System::SystemServices::DLL_PROCESS_ATTACH;
@@ -11,16 +9,13 @@ fn main() {
     debug::init();
     debug::info("GrimMod attached to GrimFandango.exe");
 
-    match Process::init() {
-        Some(process) => {
-            debug::info(format!(
-                "Base memory address found: {:x}",
-                process.base_address
-            ));
-        }
-        None => {
-            debug::error("Could not find base memory address for GrimFandango.exe");
-        }
+    if process::init() {
+        debug::info(format!(
+            "Base memory address found: {:x}",
+            process::base_address()
+        ));
+    } else {
+        debug::error("Could not find base memory address for GrimFandango.exe");
     };
 }
 
--- src/process.rs
+++ src/process.rs
@@ -1,3 +1,4 @@
+use std::sync::Mutex;
 use windows::core::PCWSTR;
 use windows::Win32::Foundation::BOOL;
 use windows::Win32::System::LibraryLoader::GetModuleHandleW;
@@ -7,63 +8,65 @@ use windows::Win32::System::Memory::{
 use windows::Win32::System::ProcessStatus::{GetModuleInformation, MODULEINFO};
 use windows::Win32::System::Threading::GetCurrentProcess;
 
-pub struct Process {
-    pub base_address: usize,
+static BASE_ADDRESS: Mutex<usize> = Mutex::new(0);
+
+pub fn init() -> bool {
+    match calculate_base_address() {
+        Some(base_address) => {
+            *BASE_ADDRESS.lock().unwrap() = base_address;
+            true
+        }
+        None => false,
+    }
 }
 
-impl Process {
-    pub fn new(base_address: usize) -> Process {
-        Process { base_address }
-    }
-
-    pub fn init() -> Option<Process> {
-        Process::get_base_address().map(Process::new)
-    }
-
-    pub fn get_base_address() -> Option<usize> {
-        unsafe {
-            let process_handle = GetCurrentProcess();
-            let hmodule = GetModuleHandleW(PCWSTR::null()).ok()?;
-            let mut module_info = std::mem::zeroed::<MODULEINFO>();
-            let result = GetModuleInformation(
-                process_handle,
-                hmodule,
-                &mut module_info as *mut MODULEINFO,
-                std::mem::size_of::<MODULEINFO>() as u32,
-            );
-            (result == BOOL(1)).then_some(module_info.lpBaseOfDll as usize)
-        }
-    }
-
-    pub unsafe fn read<T: Sized + Copy>(&self, address: usize) -> T {
-        let fixed = self.base_address + address;
-        *(fixed as *const T)
-    }
-
-    pub unsafe fn with_mut_ref<T, F: FnOnce(&mut T)>(&self, address: usize, block: F) {
-        let fixed = self.base_address + address;
-        let mut existing_flags: PAGE_PROTECTION_FLAGS = std::mem::zeroed();
-        VirtualProtect(
-            address as *mut _,
-            std::mem::size_of::<T>(),
-            PAGE_EXECUTE_READWRITE,
-            &mut existing_flags,
+pub fn calculate_base_address() -> Option<usize> {
+    unsafe {
+        let process_handle = GetCurrentProcess();
+        let hmodule = GetModuleHandleW(PCWSTR::null()).ok()?;
+        let mut module_info = std::mem::zeroed::<MODULEINFO>();
+        let result = GetModuleInformation(
+            process_handle,
+            hmodule,
+            &mut module_info as *mut MODULEINFO,
+            std::mem::size_of::<MODULEINFO>() as u32,
         );
-
-        let value = &mut *(fixed as *mut T);
-        block(value);
-
-        VirtualProtect(
-            address as *mut _,
-            std::mem::size_of::<T>(),
-            existing_flags,
-            &mut existing_flags,
-        );
+        (result == BOOL(1)).then_some(module_info.lpBaseOfDll as usize)
     }
+}
 
-    pub unsafe fn write<T: Sized>(&self, address: usize, value: T) {
-        self.with_mut_ref(address, |reference| {
-            *reference = value;
-        });
-    }
+pub fn base_address() -> usize {
+    *BASE_ADDRESS.lock().unwrap()
+}
+
+pub unsafe fn read<T: Sized + Copy>(address: usize) -> T {
+    let fixed = base_address() + address;
+    *(fixed as *const T)
+}
+
+pub unsafe fn with_mut_ref<T, F: FnOnce(&mut T)>(address: usize, block: F) {
+    let fixed = base_address() + address;
+    let mut existing_flags: PAGE_PROTECTION_FLAGS = std::mem::zeroed();
+    VirtualProtect(
+        fixed as *mut _,
+        std::mem::size_of::<T>(),
+        PAGE_EXECUTE_READWRITE,
+        &mut existing_flags,
+    );
+
+    let value = &mut *(fixed as *mut T);
+    block(value);
+
+    VirtualProtect(
+        fixed as *mut _,
+        std::mem::size_of::<T>(),
+        existing_flags,
+        &mut existing_flags,
+    );
+}
+
+pub unsafe fn _write<T: Sized>(address: usize, value: T) {
+    with_mut_ref(address, |reference| {
+        *reference = value;
+    });
 }
